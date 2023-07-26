4ddfec95c4bdd7b0fdb267adca2a95dc6bfbc581
Pablo Bustos <pablobustos609@gmail.com>
2023-07-26 08:34:08 -0300

    added getAllTasks

--- task-api/controllers/tasks.js
+++ task-api/controllers/tasks.js
@@ -2,16 +2,26 @@ const Task = require("../models/Task")
 
 module.exports = {
   getAllTasks: async(req, res) => {
-    res.send("get tasks")
+    try{
+      const tasks = await Task.find({}, {name:1, completed: 1})
+  
+      res.json({tasks})
+    }catch(e){
+      res.status(500).json(e)
+    }
   },
 
   createTask: async(req, res) => {
-    const task = await Task.create(req.body)
-    
-    res.status(201).json({
-      status: "created",
-      task
-    })
+    try{
+      const task = await Task.create(req.body)
+
+      res.status(201).json({
+        status: "created",
+        task
+      })
+    }catch(e){
+      res.status(500).json(e)
+    }
   },
 
   getTask: (req, res) => {
--- task-api/models/Task.js
+++ task-api/models/Task.js
@@ -1,8 +1,15 @@
 const { Schema, model } = require("mongoose")
 
 const Task = new Schema({
-  name: String,
-  completed: Boolean
+  name: {
+    type: String,
+    required: [true, "name is required"],
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
 })
 
 module.exports = model("Task", Task)
